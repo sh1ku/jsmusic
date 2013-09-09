bf9dd4086171ffba8613199718ddc3e7e98870e1
Alberto Salazar <alberto240995@gmail.com>
2013-09-09 16:40:05 +0200

    Added index.php
    Editing content...

---
+++ index.php
@@ -0,0 +1,62 @@
+<table class='fullTable'>
+
+  <tbody>
+    <tr style='vertical-align:top;'>
+    	<td>
+		<table class='mainTable'>
+    		<thead>
+            	<?
+               		drawTHead();
+				?>
+			</thead>
+   			<tbody class='mainTBody'>
+    			<?
+					drawTBody();
+				?>  
+			</tbody>
+		</table>
+    	</td>
+        <td>
+        	<table>
+            	<thead>
+                <tr id="void" style="padding-bottom:20px;">
+                <th>
+                <br />
+                </th>
+                </tr>
+          		<tr class="listenRow">
+                <th>
+   				<?
+					drawMusicControl();
+				?>
+            	</th>
+            	</tr>
+            </thead>
+            <tbody>
+            	<tr>
+                <td class='aboutRow'>
+                	<div>
+                    	R-ESMUT  és un projecte radiofònic
+                        de l’escola ESMUT que vol
+                        expressar el seu interès pels 
+                        diferents àmbits de la música, 
+                        es a dir, la docència, 
+                        la interpretació, la creació i 
+                        la gestió. 
+                        <br/>
+                        <br/>
+                        Tanmateix volem ser vehicle de 
+                        la veu dels nostres alumnes i 
+                        docents. Els programes de 
+                        R-ESMUT estan disponibles per 
+                        a l’escolta a la carta
+                        i per descàrrega.
+					</div>
+                </td>
+                </tr>
+            </tbody>
+            </table>
+        </td>
+    </tr>
+    </tbody>
+</table>
